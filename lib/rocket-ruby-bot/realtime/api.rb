f53084ae63ef0e3e74297fde8b8c4ab892b159a0
Nicolas Chuche <nicolas@barna.be>
2018-09-01 16:46:14 +0200

    more methods and better error handling

--- lib/rocket-ruby-bot/realtime/api.rb
+++ lib/rocket-ruby-bot/realtime/api.rb
@@ -2,17 +2,40 @@ module RocketRubyBot
   module Realtime
     module API
 
-      class UnknownStatus < StandardError; end
+      class ArgumentError < StandardError; end
+      class UnknownArgumentValue < StandardError; end
+
+      KNOWN_STATUS = %W<online busy away offline>.freeze
+      CHANNEL_TYPE = %w[public private].freeze
+      SORT_TYPE    = %w[name msgs].freeze
       
       def self.included(base)
         base.extend(ClassMethods)
       end
 
       module ClassMethods
+
+        def send_login(options)
+          if options.has_key? :digest and options.has_key? :username
+            return {msg: 'method',
+                    method: 'login',
+                    params: [{ user: { username: options[:username] },
+                               password: { digest: options[:digest], algorithm: "sha-256" }}]}
+          elsif options.has_key? :token
+            return {msg: "method",
+                    method: "login",
+                    params: [ { resume: options[:token] }]}
+          end
+          raise ArgumentError, "should have (digest and username) or token"
+        end
         
         def send_text_message(message_id:, room_id:, msg:)
           {msg: 'method',method: 'sendMessage',
            'params': [{message_id: uuid, rid: room_id, msg: msg}]}
+        end
+
+        def send_pong
+          {msg: 'pong'}
         end
         
         def sub_stream_room_messages(room_id:)
@@ -29,11 +52,31 @@ module RocketRubyBot
         end
         
         def set_presence(status:)
-          raise UnknownStatus unless %W<online busy away offline>.include? status
+          raise UnknownArgumentValue, "should be in [#{KNOWN_STATUS.join(" ")}]" unless KNOWN_STATUS.include? status
           {msg: "method", method: "UserPresence:setDefaultStatus",
            params: [status]}
         end
+
+        # methods found at https://github.com/mathieui/unha2/blob/master/docs/methods.rst
         
+        def get_room_id(room:)
+          {msg: "method", method: "getRoomIdByNameOrId",params: [room]}
+        end
+
+        def get_channels_list(filter: "", type: "public", sort_by: "name", limit: 500)
+          raise UnknownArgumentValue, "should be in [#{CHANNEL_TYPE.join(" ")}]" unless CHANNEL_TYPE.include? type
+          raise UnknownArgumentValue, "should be in [#{SORT_TYPE.join(" ")}]"   unless SORT_TYPE.include? sort_by
+
+          {msg: "method", method: "channelsList", params: [filter, type, limit, sort_by]}
+        end
+        
+        def get_users_of_room(room_id:, user_status: "true")
+          {msg: "method", method: "getUsersOfRoom", params: [room_id, user_status]}
+        end
+
+        def read_messages
+          {msg: "method", method: "readMessages", params: []}
+        end
       end
     end
   end
