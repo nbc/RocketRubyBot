8e03bafad6990e8b87cfc9d95ebdf3c33d904e83
Nicolas Chuche <nicolas@barna.be>
2018-09-01 10:33:34 +0200

    ajout de methodes d'API RC

--- lib/rocket-ruby-bot/realtime/api.rb
+++ lib/rocket-ruby-bot/realtime/api.rb
@@ -10,16 +10,24 @@ module RocketRubyBot
 
       module ClassMethods
         
-        def send_text_message(id:, room_id:, msg:)
+        def send_text_message(message_id:, room_id:, msg:)
           {msg: 'method',method: 'sendMessage',
-           'params': [{id: uuid, rid: room_id, msg: msg}]}
+           'params': [{message_id: uuid, rid: room_id, msg: msg}]}
         end
         
         def sub_stream_room_messages(room_id:)
           {msg: "sub", name: "stream-room-messages",
            params: [room_id,false]}
         end
+        
+        def sub_stream_notify_all
+          {msg: "sub", name: "stream-notify-all", params:["event", false]}
+        end
 
+        def sub_stream_notify_logged
+          {msg: "sub",name: "stream-notify-logged", params:["event", false]}
+        end
+        
         def set_presence(status:)
           raise UnknownStatus unless %W<online busy away offline>.include? status
           {msg: "method", method: "UserPresence:setDefaultStatus",
