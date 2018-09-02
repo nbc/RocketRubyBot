22423e6c0395eb31791ebbf5f37c4f76321642e9
Nicolas Chuche <nicolas@barna.be>
2018-09-02 11:56:52 +0200

    implement say with a block to wait for the response

--- lib/rocket-ruby-bot/realtime/client.rb
+++ lib/rocket-ruby-bot/realtime/client.rb
@@ -1,12 +1,14 @@
 module RocketRubyBot
   module Realtime
     class Client
-
       class NoValidURL < StandardError; end
       
       include RocketRubyBot::MessageId
       
       attr_accessor :hooks
+
+      # FIXME
+      @@fiber_store = Hash.new
 
       def self.instance
         @instance ||= new
@@ -17,16 +19,36 @@ module RocketRubyBot
         @web_socket = nil
       end
 
-      def say(args = {})
-        id = next_id
-        args = {id: id}.merge(args)
+      def say(args = {}, id = true, &block)
+
+        if id
+          id = next_id
+          args = {id: id}.merge(args)
+        end
+
         p [ "-> ", args]
-        @web_socket.send(args.to_json)
+        if !block_given?
+          @web_socket.send(args.to_json)
+        else
+          f = Fiber.new do
+            @web_socket.send(args.to_json)
+            message = Fiber.yield
+            block.call message
+          end
+          @@fiber_store[id] = f
+          f.resume
+        end
       end
 
       def dispatch_messages(data)
         return unless data._type
         type = data._type
+
+        if @@fiber_store.has_key? data.uid
+          @@fiber_store[data.uid].resume data
+          @@fiber_store.delete data.uid
+        end
+
         return unless @hooks[type]
         hooks[type].each do |hook|
           hook.call(self, data)
@@ -47,7 +69,7 @@ module RocketRubyBot
 
           @web_socket.on :open do |event|
             p [:open]
-            say({"msg" => "connect", "version" => "1", "support" =>  ["1"]})
+            @web_socket.send({"msg" => "connect", "version" => "1", "support" =>  ["1"]}.to_json)
           end
 
           @web_socket.on :message do |event|
