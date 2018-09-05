5c625f87ac4dd757d7f11ec2f62f2338bcd24dc0
Nicolas Chuche <nicolas@barna.be>
2018-09-05 07:37:52 +0200

    authentication failure handling

--- lib/rocket-ruby-bot/hooks/base.rb
+++ lib/rocket-ruby-bot/hooks/base.rb
@@ -2,6 +2,7 @@ module RocketRubyBot
   module Hooks
     class Base
       include RocketRubyBot::Realtime::API
+      include RocketRubyBot::Loggable
     end
   end
 end
--- lib/rocket-ruby-bot/hooks/connected.rb
+++ lib/rocket-ruby-bot/hooks/connected.rb
@@ -8,6 +8,10 @@ module RocketRubyBot
       
       def call(client, data)
         client.say send_login(token: @config.token) do |message|
+          if message.error and message.error.error == 403
+            logger.fatal "authentication failed"
+            raise "authentication failed"
+          end
           @config.user_id = message.result['id']
         end
       end
