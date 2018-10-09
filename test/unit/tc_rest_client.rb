ff1dfd7f4e4206aae11a7e9626da80941163f724
Nicolas Chuche <nicolas@barna.be>
2018-10-09 20:33:42 +0200

    add test for rest client

---
+++ test/unit/tc_rest_client.rb
@@ -0,0 +1,19 @@
+require_relative '../test_helpers'
+
+require 'json'
+require 'rocket-ruby-bot/rest/client'
+
+class TestEvents < MiniTest::Test
+  
+  def test_rest_client
+    client = RocketRubyBot::Rest::Client.session(url: 'url', token: 'token', user_id: 'id')
+   
+    assert_instance_of RocketChat::Session, client
+
+    assert_equal 'url',   client.server.server.to_s
+    assert_equal 'token', client.token.auth_token
+    assert_equal 'id',    client.token.user_id
+
+    assert_respond_to client, :groups
+  end
+end
