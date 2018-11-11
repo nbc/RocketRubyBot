2492a8c8ae87b52eefa36183129cdb235ed1b060
Nicolas Chuche <nicolas@barna.be>
2018-11-11 15:47:28 +0100

    add some examples

---
+++ examples/README.md
@@ -0,0 +1,6 @@
+To test those scripts :
+
+1. install docker image of rocketchat with [this documentation](https://hub.docker.com/_/rocket.chat/)
+2. create a bot 'mybot' with password 'mybot'
+
+Some scripts need more right than normal bot rights
---
+++ examples/pong.rb
@@ -0,0 +1,30 @@
+# coding: utf-8
+path = File.expand_path( File.join( File.dirname( __FILE__) , "..", "lib"))
+$:.unshift path
+
+require 'rocket-ruby-bot'
+
+RocketRubyBot.configure do |config|
+  config.url    = 'http://localhost:3000'
+  config.user   = 'mybot'
+  config.digest  = '2e6669346178ab7d3edb94d7141082c0ba481e52a21d94ac422c6b68a99b34fc' # mybot
+end
+
+RocketRubyBot::UserStore.configure do |user_store|
+  user_store.room_name = 'general'
+end
+
+class PongBot < RocketRubyBot::Bot
+  setup do |client|
+    client.say get_room_id(room: user_store.room_name) do |message|
+      user_store.room_id = message.result
+      client.say sub_stream_room_messages(room_id: user_store.room_id)
+    end
+  end
+
+  command 'ping' do |client, message, match|
+    client.say send_message(message_id: uuid, room_id: message.rid, msg: "pong")
+  end
+end
+
+PongBot.run
