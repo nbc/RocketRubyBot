84f567d64d900863cdfc45f9862acae218016d85
Nicolas Chuche <nicolas@barna.be>
2018-11-24 13:46:32 +0100

    test to verify Stream classes respond to type

--- test/unit/tc_realtime_events_stream_notify.rb
+++ test/unit/tc_realtime_events_stream_notify.rb
@@ -5,27 +5,28 @@ require 'json'
 require 'rocket-ruby-bot/realtime/events'
 
 class TestStreamNotify < MiniTest::Test
+
+  def setup
+    @obj = OpenStruct.new(fields: OpenStruct.new(eventName: 'truc'))
+  end
+  
   def test_stream_notify_logged
-    obj = OpenStruct.new(fields: OpenStruct.new(eventName: 'truc'))
-    stream = RocketRubyBot::Realtime::Events::StreamNotifyLogged.new obj
-    assert_equal :truc, stream.type
+    stream = RocketRubyBot::Realtime::Events::StreamNotifyLogged.new @obj
+    assert stream.type
   end
 
   def test_stream_notify_all
-    obj = OpenStruct.new(fields: OpenStruct.new(eventName: 'truc'))
-    stream = RocketRubyBot::Realtime::Events::StreamNotifyAll.new obj
-    assert_equal :truc, stream.type
+    stream = RocketRubyBot::Realtime::Events::StreamNotifyAll.new @obj
+    assert stream.type
   end
 
   def test_stream_notify_user
-    obj = OpenStruct.new(fields: OpenStruct.new(eventName: 'machin/tr-uc'))
-    stream = RocketRubyBot::Realtime::Events::StreamNotifyUser.new obj
-    assert_equal :tr_uc, stream.type
+    stream = RocketRubyBot::Realtime::Events::StreamNotifyUser.new @obj
+    assert stream.type
   end
 
   def test_stream_notify_room
-    obj = OpenStruct.new(fields: OpenStruct.new(eventName: 'machin/tr-uc'))
-    stream = RocketRubyBot::Realtime::Events::StreamNotifyRoom.new obj
-    assert_equal :tr_uc, stream.type
+    stream = RocketRubyBot::Realtime::Events::StreamNotifyRoom.new @obj
+    assert stream.type
   end
 end
