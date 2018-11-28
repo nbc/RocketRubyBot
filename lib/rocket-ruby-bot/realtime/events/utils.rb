45db5c20d1a346633169bb38e4e4e9ed42ec2e99
Nicolas Chuche <nicolas@barna.be>
2018-11-28 19:24:52 +0100

    cleaning

--- lib/rocket-ruby-bot/realtime/events/utils.rb
+++ lib/rocket-ruby-bot/realtime/events/utils.rb
@@ -14,8 +14,7 @@ module RocketRubyBot
         def to_snake_case(name)
           name.gsub(/([A-Z]+)([A-Z][a-z])/,'\1_\2').
             gsub(/([a-z\d])([A-Z])/,'\1_\2').
-            tr("-", "_").
-            tr(':', '_').
+            tr("-:", "_").
             downcase
         end
         
